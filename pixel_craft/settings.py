46474a5a69b0a5e7ff1507058c097f29b11e41a5
Hayden Caldwell <hade_caldwell@msn.com>
2025-05-15 10:31:42 +0000

    Add middleware for Whitenoise

--- pixel_craft/settings.py
+++ pixel_craft/settings.py
@@ -80,6 +80,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
 ROOT_URLCONF = 'pixel_craft.urls'
